a231ab3d99877dac8fe0f905c8650449fc111f21
suyu <huangsuyu@huangsuyudeMacBook-Pro.local>
2020-12-22 17:44:33 +0800

    edit readme

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# verify-example
+# Verify your contract on Binance Smart Chain
+
+
+1. Install the plugin with npm
+
+```
+npm install -D truffle-plugin-verify
+```
+
+Make sure the version is >=`v 0.5.4`
+
+2. Add the plugin to your truffle-config.js file
+```
+module.exports = {
+  /* ... rest of truffle-config */
+
+  plugins: [
+    'truffle-plugin-verify'
+  ]
+}
+```
+3. Generate an API Key on your BSCscan account
+
+If you don't have one yet, just go to [this page](https://bscscan.com/login) to sign up.
+
+
+Add your BSCscan API key to your truffle config
+
+```
+module.exports = {
+  /* ... rest of truffle-config */
+
+  api_keys: {
+    bscscan: 'MY_API_KEY'
+  }
+}
+```
+
+4.  Deploy your contract
+
+```
+truffle compile
+truffle migrate --network testnet
+```
+
+5. Verify your contract
+
+```
+ truffle run verify BEP20Token@{deployed-address} --network testnet
+
+```
--- truffle-config.js
+++ truffle-config.js
@@ -41,38 +41,8 @@ module.exports = {
     bscscan: BSCSCANAPIKEY
   },
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    development: {
-     host: "https://data-seed-prebsc-2-s3.binance.org",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
-     network_id: 97      // Any network (default: none)
-    },
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-    // Useful for private networks
-    live: {
+
+    testnet: {
         provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
         network_id: 97,   // This network is yours, in the cloud.
         timeoutBlocks: 200,
